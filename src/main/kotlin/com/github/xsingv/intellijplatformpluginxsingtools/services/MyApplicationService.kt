e690f28c5854fd7730e9559e16ef70d14ad31271
GitHub Action <action@github.com>
2022-11-03 06:25:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/xsingv/intellijplatformpluginxsingtools/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xsingv/intellijplatformpluginxsingtools/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xsingv.intellijplatformpluginxsingtools.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xsingv.intellijplatformpluginxsingtools.MyBundle
 
 class MyApplicationService {
 
